02d94cff96ff0cc1284fd17d14c589fdb7ee9a2d
LAGstudi0 <paolo.milutinovic@gmail.com>
2023-11-24 15:29:02 +0000

    minor changes done to text info

--- src/containerRight.js
+++ src/containerRight.js
@@ -14,16 +14,16 @@ function rightSection() {
     //different parragraps
     const introP1 = document.createElement('p');
     introP1.classList.add('intro-p');
-    introP1.innerHTML = 'Back in 2012, I decided to try my hand at creating custom Tumblr themes and tumbled head first into the rabbit hole of coding and web development. Fast-forward to today, and I’ve had the privilege of building software for an advertising agency, a start-up, a student-led design studio, and a huge corporation.';
+    introP1.innerHTML = 'Back in 2015 I decided to start my coding journey by joining to a coding competition with some friends, that we ended up winning. Going through the development process I tumbled head first into the rabbit hole of coding and user interface. Fast forward to today, and I’ve had the privilege of building software for small business and managed to complete a Computer Science bachelors degree.';
 
     const introP2 = document.createElement('p');
     introP2.classList.add('intro-p');
-    introP2.innerHTML = 'My main focus these days is building products and leading projects for our clients at Upstatement. In my free time I\'ve also released an online video course that covers everything you need to know to build a web app with the Spotify API.';
+    introP2.innerHTML = 'My main focus these days is gaining more experience in order to produce a better quality code. In my free time I also like to work in my own projects in order to express my creativity and enhance my knowledge.';
 
 
     const introP3 = document.createElement('p');
     introP3.classList.add('intro-p');
-    introP3.innerHTML = 'When I’m not at the computer, I’m usually rock climbing, hanging out with my wife and two cats, or running around Hyrule searching for korok seeds.'
+    introP3.innerHTML = 'When I’m not at the computer, I’m usually playing football, hanging out with my girlfriend or thinking about code.'
 
     //append parragraphs to container.
     introContainer.appendChild(introP1);
